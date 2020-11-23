a64caf384922ab6c5133a1a63cad32313b1b91ea
Brijesh <wani@ityx.de>
2020-11-24 00:09:40 +0530

    Some examples.

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+//User: BrijeshWani
+//Hint: J*0
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
