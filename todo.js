f0cdbd953020694a0ec2a97ce8bcf4bf9ff7d142
Brijesh <wani@ityx.de>
2020-11-27 22:14:25 +0530

    Some practice examples.

--- todo.js
+++ todo.js
@@ -1,6 +1,4 @@
 //User: BrijeshWani
 //Hint: J*0
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this //Bind function
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
