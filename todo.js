a970ef952476a09c897ce03f8d538ab278f931d1
Brijesh <wani@ityx.de>
2020-11-28 17:07:11 +0530

    Study examples

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
 //User: BrijeshWani
 //Hint: J*0
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
